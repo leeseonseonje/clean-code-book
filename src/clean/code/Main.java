bb004b787ef773657a798c6df18556c642266df6
leeseonseonje <leeseonje9323@gmail.com>
2021-11-17 20:18:36 +0900

    m

---
+++ src/clean/code/Main.java
@@ -0,0 +1,8 @@
+package clean.code;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/clean/code/의미있는이름/의도를_분명히_밝혀라.java
@@ -0,0 +1,5 @@
+package clean.code.의미있는이름;
+
+public class 의도를_분명히_밝혀라 {
+    
+}
