6380499b1f6322bdf5f601b9643dddd7880feaa6
leeseonseonje <leeseonje9323@gmail.com>
2021-11-23 21:12:33 +0900

    2021-11-23, new Computer Commit Test

--- src/clean/code/Main.java
+++ src/clean/code/Main.java
@@ -3,6 +3,6 @@ package clean.code;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("new Computer Commit Test");
+        System.out.println("2021-11-23, new Computer Commit Test");
     }
 }
